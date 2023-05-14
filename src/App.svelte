68115efd7ab0d16e51c05a767579ec446fccc09e
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-05-14 20:20:38 +0200

    Integrated FA

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,16 @@
   import "@fontsource/raleway"
 
   import NewEventModal from './components/new-event-modal.svelte'
+  import FontAwesomeIcon from "./components/fontawesome-icon.svelte";
+
+  // use the font awesome icon component
+  // <FontAwesomeIcon icon="fa-solid fa-calendar-plus" />
+
 </script>
 
 <main>
   <div class="card">
+    <!-- <FontAwesomeIcon icon="fa-solid fa-calendar-plus" /> -->
     <NewEventModal />
   </div>
 </main>
