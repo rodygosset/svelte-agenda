f9fe03683dcc7d3e45e3b35d614df7ddb7547a5b
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-05-15 07:31:20 +0200

    Changes

--- src/App.svelte
+++ src/App.svelte
@@ -3,38 +3,9 @@
 	import "@fontsource/raleway"
 	import Header from "./components/layout/header.svelte";
     import DayCell from "./components/calendar/day-cell.svelte";
-    import { currentMonth } from "./stores/current-month-store";
+    import { currentMonth, getDatesInCurrentMonth } from "./stores/current-month-store";
 
 	const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-	const getDatesInCurrentMonth = (currentMonth: Date) => {
-		const dates: Date[] = [];
-		const year = currentMonth.getFullYear();
-		const month = currentMonth.getMonth();
-		const firstDay = new Date(year, month, 1);
-		const lastDay = new Date(year, month + 1, 0);
-		const daysInMonth = lastDay.getDate();
-		const firstDayIndex = firstDay.getDay();
-		const lastDayIndex = lastDay.getDay();
-		const nextDays = 7 - lastDayIndex - 1;
-
-		for (let i = firstDayIndex; i > 0; i--) {
-			dates.push(undefined);
-		}
-
-		for (let i = 1; i <= daysInMonth; i++) {
-			// add the current date to the list
-			dates.push(new Date(year, month, i));
-		}
-
-		for (let i = 1; i <= nextDays; i++) {
-			dates.push(undefined);
-		}
-
-		return dates;
-	}
-
-	$: datesInMonth = getDatesInCurrentMonth($currentMonth)
 
 
 	// rendering strategy:
@@ -42,7 +13,14 @@
 	// 2. if it's anything but a monday, render the previous days of the week, as empty cells
 	// 3. keep rendering until the end of the month
 	// 4. if the last day of the month is not a sunday, render the next days of the week, as empty cells
-	// Important: use a grid with a width of 7 columns, and a height of 6 rows, that automatically wraps the content
+	// Important: use a grid with a width of 7 columns that automatically wraps the content
+
+	// ==> the getDatesInCurrentMonth function takes care of this logic
+
+
+	$: datesInMonth = getDatesInCurrentMonth($currentMonth)
+
+
 
 
 </script>
